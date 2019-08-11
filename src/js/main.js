ba6709037db1369e06910ba4d6056175d441010c
Piotr Napieralski <piotrn-87@o2.pl>
2019-08-11 14:13:44 +0200

    Feat: starting with JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,32 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+const list = document.querySelector('.projects__items--js');
+const button = document.querySelector('.button__more--js');
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+fetch('https://api.github.com/users/piotrn-87/repos?sort=updated&direction=desc')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repository of repos) {
+      const {
+        name,
+        description,
+        homepage,
+        html_url
+      } = repository;
+      list.innerHTML +=
+        `<li class="projects__item">
+        <div class="projects__wrapper">
+        <img class="projects__logo" src="assets/img/github.svg" alt="github icon">
+        <h3 class="projects__name">${name}</h3> 
+        ${description ? `<p class="projects__description">${description}</p>`: `no description`
+        }
+        </div>
+            <a class="projects__link" 
+            href="${repository.html_url}"> ${repository.name}</a></li>`;
+    }
+  })
 
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
+  .catch(error => {
+    console.log('cant find github API');
+  })
