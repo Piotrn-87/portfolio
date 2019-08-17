8d9e3180d571b918d005fc2ae655f2b0dd21c6ce
Piotr Napieralski <piotrn-87@o2.pl>
2019-08-17 18:13:35 +0200

    Fix: Refactor style and JS code

--- src/js/main.js
+++ src/js/main.js
@@ -3,44 +3,11 @@
 const list = document.querySelector('.container__projects--js');
 const button = document.querySelector('.button__load--js');
 
-// fetch('https://api.github.com/users/piotrn-87/repos?sort=updated&direction=desc')
-//   .then(resp => resp.json())
-//   .then(resp => {
-//     const repos = resp;
-
-//     for (const repository of repos) {
-//       const {
-//         name,
-//         description,
-//         homepage,
-//         html_url
-//       } = repository;
-//       list.innerHTML +=
-//         `<li class="project">
-//           <div class="project__wrapper">
-//           <img class="project__logo" src="assets/img/github.svg" alt="github image">
-//           <h3 class="project__name">${name}</h3> 
-//           ${description ? `<p class="project__description">${description}</p>`: 'no description'
-//           }
-//           </div>
-//           <div class="project__footer">
-//           ${homepage ? `<a class="project__link project__link--active" href="${homepage}" target="_blank" title="Demo : ${name}">Demo</a>` : ''
-//           }
-//           <a class="project__link" href="${html_url}" target="_blank" title="source : ${name}">Github</a>
-//           </div>
-//         </li>
-//         `;
-//     }
-//   })
-//   .catch(error => {
-//     console.log('cant find github API');
-//   })
-
 const loadRepo = () => {
   fetch("https://api.github.com/users/piotrn-87/repos?sort=updated&direction=desc")
     .then(resp => resp.json())
     .then(resp => {
-      localStorage.setItem('Data', JSON.stringify(resp))
+      localStorage.setItem('data', JSON.stringify(resp))
       repositories(resp.slice(0, 4));
     })
     .catch((error) => {
@@ -48,15 +15,15 @@ const loadRepo = () => {
     });
 }
 
-const repositories = (e) => {
-  if (e.length) {
-    let tempData = e;
+const repositories = (repo) => {
+  if (repo.length) {
+    let tempData = repo;
     list.innerHTML = '';
-    let moreData = e.length + 4;
+    let moreData = repo.length + 4;
     button.addEventListener('click', () => {
-      let tempData = JSON.parse(localStorage.getItem('Data')).slice(0, moreData)
-      if (moreData >= JSON.parse(localStorage.getItem('Data')).length) {
-        buttonMore.style.display = 'none';
+      let tempData = JSON.parse(localStorage.getItem('data')).slice(0, moreData)
+      if (moreData >= JSON.parse(localStorage.getItem('data')).length) {
+        button.style.display = 'none';
       }
       repositories(tempData)
     })
